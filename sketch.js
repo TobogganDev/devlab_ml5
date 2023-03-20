b69c6daad115131e8686966003a426f05d019d8b
jarrel <jarrel.folly-adjon@edu.devinci.fr>
2023-03-20 12:50:41 +0100

    test

--- sketch.js
+++ sketch.js
@@ -1,11 +1,21 @@
 let img;
-
+let detector;
 
 function preload(){
-  img = loadImage('https://alpha.aeon.co/images/acd6897d-9849-4188-92c6-79dabcbcd518/header_essay-final-gettyimages-685469924.jpg')
+  img = loadImage('https://alpha.aeon.co/images/acd6897d-9849-4188-92c6-79dabcbcd518/header_essay-final-gettyimages-685469924.jpg');
+  detector = ml5.ObejectDetector('cocossd');
+}
+
+function gotDetections(error, results){
+  if (error){
+    console.error(error);
+  }
+  console.log(results);
+
 }
 
 function setup(){
   creatCanvas(640, 480);
   Image(img,0, 0);
+  detector.detect(img, gotDetections);
 }
