c0d497c35e6143b99d1398dea17cd369b16f3f84
jarrel <jarrel.folly-adjon@edu.devinci.fr>
2023-03-20 12:52:02 +0100

    test js work ?

--- sketch.js
+++ sketch.js
@@ -1,21 +1,35 @@
 let img;
 let detector;
 
-function preload(){
-  img = loadImage('https://alpha.aeon.co/images/acd6897d-9849-4188-92c6-79dabcbcd518/header_essay-final-gettyimages-685469924.jpg');
-  detector = ml5.ObejectDetector('cocossd');
+function preload() {
+  img = loadImage('1.jpg');
+  detector = ml5.objectDetector('cocossd');
 }
 
-function gotDetections(error, results){
-  if (error){
+function gotDetections(error, results) {
+  if (error) {
     console.error(error);
   }
-  console.log(results);
-
+  console.log(results);  
+  for (let i = 0; i < results.length; i++) {
+    let object = results[i];
+    stroke(0,255,0);
+    strokeWeight(4);
+    noFill();
+    rect(object.x, object.y, object.width, object.height);
+    noStroke();
+    fill(255);
+    textSize(24);
+    text(object.label, object.x + 10, object.y+24);
+  }
+  
+  
 }
 
-function setup(){
-  creatCanvas(640, 480);
-  Image(img,0, 0);
+
+function setup() {
+  createCanvas(640, 480);
+  // console.log(detector);
+  image(img, 0, 0);
   detector.detect(img, gotDetections);
 }
